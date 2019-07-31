cc2d28cdebccfc69270f29b336646ba7db995559
github username <github email address>
2019-07-31 14:10:08 -0500

    useEffect hook to setInterval to increment the sec state once the start states gets set to true

--- client/src/components/timer.js
+++ client/src/components/timer.js
@@ -1,18 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Timer(props) {
   const [sec, setSec] = useState(0);
+  const [start, setStart] = useState(false);
+
+  // function callBack() {
+  //   setSec(sec + 1);
+  // }
+
+  useEffect(() => {
+    let interval = null;
+    const callBack = () => setSec(sec + 1);
+    if (start === true) {
+      interval = setInterval(callBack, 1000)
+    }
+
+
+    return () => clearInterval(interval);
+  }, [sec, start]
+  );
 
   function handleClick(e) {
-    const newCount = sec + 1
+    setSec(sec + 1);
+    setStart(true);
+  }
 
-    setSec(newCount);
+  function handleStop(e) {
+    setStart(false);
   }
 
   return (
     <div>
       <button onClick={handleClick}>Start</button>
       {sec}
+      <button onClick={handleStop}>Stop</button>
     </div>
   )
 }
