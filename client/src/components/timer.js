5183fee13aaffe89c67f316be10cd0c10b0f862e
github username <jtwerenski@gmail.com>
2019-08-02 20:52:37 -0500

    create a reset and toggle function and add logic to useEffect to handle when active is true and sec does not equal 0

--- client/src/components/timer.js
+++ client/src/components/timer.js
@@ -2,23 +2,27 @@ import React, { useState, useEffect } from 'react';
 
 export default function Timer(props) {
   const [sec, setSec] = useState(0);
-  const [start, setStart] = useState(false);
   const [active, setActive] = useState(false);
 
-  // function callBack() {
-  //   setSec(sec + 1);
-  // }
+  function toggle() {
+    setActive(!active);
+  }
+
+  function reset() {
+    setSec(0);
+    setActive(false);
+  }
 
   useEffect(() => {
     let interval = null;
     const callBack = () => setSec(sec + 1);
-    if (start === true) {
+    if (active) {
       interval = setInterval(callBack, 1000)
+    } else if (!active && sec !== 0) {
+      clearInterval(interval);
     }
-
-
     return () => clearInterval(interval);
-  }, [sec, start]
+  }, [sec, active]
   );
 
   function handleClick(e) {
